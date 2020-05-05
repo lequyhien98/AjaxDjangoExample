6d31274fb14f191c79f5a8b6e586f49f2a559314
lequyhien98 <lequyhien29081998@gmail.com>
2020-05-05 09:25:32 +0700

    first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\django\sử dụng ajax đơn giản với django\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ mysite/myapp/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.core import serializers
+from .forms import FriendForm
+from .models import Friend
+
+from django.views import View
+
+# Create your views here.
+class FriendView(View):
+    form_class = FriendForm
+    template_name = "myapp/index.html"
+
+    def get(self, *args, **kwargs):
+        form = self.form_class()
+        friends = Friend.objects.all()
+        return render(self.request, self.template_name, 
+            {"form": form, "friends": friends})
+
+    def post(self, *args, **kwargs):
+        if self.request.is_ajax and self.request.method == "POST":
+            form = self.form_class(self.request.POST)
+            if form.is_valid():
+                instance = form.save()
+                ser_instance = serializers.serialize('json', [ instance, ])
+                # send to client side.
+                return JsonResponse({"instance": ser_instance}, status=200)
+            else:
+                return JsonResponse({"error": form.errors}, status=400)
+
+        return JsonResponse({"error": ""}, status=400)
+
+def checkNickName(request):
+    # request should be ajax and method should be GET.
+    if request.is_ajax and request.method == "GET":
+        # get the nick name from the client side.
+        nick_name = request.GET.get("nick_name", None)
+        # check for the nick name in the database.
+        if Friend.objects.filter(nick_name = nick_name).exists():
+            # if nick_name found return not valid new friend
+            return JsonResponse({"valid":False}, status = 200)
+        else:
+            # if nick_name not found, then user can create a new friend.
+            return JsonResponse({"valid":True}, status = 200)
+
+    return JsonResponse({}, status = 400)
